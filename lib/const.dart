a5297d01e2e217c300f9cf7e3206b80df26f98d6
Adrien <adrienrossignol1@gmail.com>
2022-03-25 17:17:01 +0100

    Add the app

---
+++ lib/const.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const d_green = Color(0xFF54D3C2);
+
+final List hotelList = [
+    {
+      'title': 'Grand Royl Hotel',
+      'place': 'wembley, London',
+      'distance': 2,
+      'review': 80,
+      'picture': 'images/hotel1.jpg',
+      'price': '180',
+    },
+    {
+      'title': 'Queen Hotel',
+      'place': 'wembley, London',
+      'distance': 2,
+      'review': 80,
+      'picture': 'images/hotel2.jpg',
+      'price': '220',
+    },
+];
